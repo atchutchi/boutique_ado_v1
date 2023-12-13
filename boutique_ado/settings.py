4f81cc08c650eec0a86ccf5663cef97b12fb5923
atchutchi <>
2023-12-13 05:27:41 +0000

    Added bag context and delivery logic

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -74,6 +74,7 @@ TEMPLATES = [
                 'django.template.context_processors.request', # require by allauth
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'bag.contexts.bag_contents',
             ],
         },
     },
@@ -160,3 +161,9 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# Stripe
+FREE_DELIVERY_THRESHOLD = 50
+STANDARD_DELIVERY_PERCENTAGE = 10
+STRIPE_CURRENCY = 'usd'
