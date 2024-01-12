9ec01bff58ea60ac0e0f54c4b940da8a11b7d658
Atchutchi Ferreira <ferreira.atchutchi@gmail.com>
2024-01-12 04:55:18 +0000

    Added stripe elements

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51OXcHzKLDCgR8flDgFF5dDOayADfhdBQNfiKV1kzaKduQDXmjhmqNDjclga32QmdwmSKjVGJ13Ek6nU0vg2ZX1RC00wSHZQ8BR',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
