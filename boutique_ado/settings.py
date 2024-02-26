b1217faaa36fb8955d4da3cbe737f5a9cd972eea
Atchutchi Ferreira <ferreira.atchutchi@gmail.com>
2024-02-26 07:23:42 +0000

    Edited settings.py

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -130,14 +130,14 @@ WSGI_APPLICATION = 'boutique_ado.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 if 'DATABASE_URL' in os.environ:
-    DATABASE_URL = {
+    DATABASES = {
         'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
 else:
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
         }
     }
 
