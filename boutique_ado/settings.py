fdfef48b5c8b9bf555ba1990d7ccf3d332f89445
Atchutchi Ferreira <ferreira.atchutchi@gmail.com>
2024-02-26 07:12:36 +0000

    Deployment part7

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -17,9 +17,9 @@ import dj_database_url
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-# BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+#BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
